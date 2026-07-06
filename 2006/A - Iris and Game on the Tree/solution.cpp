812d0503b6ef823a831ec724a112e4f1b2c9a9fc
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-06 17:24:27 +0530

    Add Iris and Game on the Tree [A] from Codeforces

---
+++ 2006/A - Iris and Game on the Tree/solution.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ll=unsigned long long;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+int lcm(int a, int b) {
+    return (a*b)/__gcd(a, b);
+}
+// Returns gcd(a, b) and updates x and y to solve: ax + by = gcd(a, b)
+int extGCD(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extGCD(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+    g= extGCD(abs(a), abs(b), x0, y0);
+    if (c%g!= 0) {
+        return false;
+    }
+    
+    x0*=c/g;
+    y0*=c/g;
+    if(a<0)x0= -x0;
+    if(b<0)y0= -y0;
+    return true;
+}
+ 
+//Topological Sort
+vector<int> topoSort(int &V, vector<vector<int>> adj){
+    int indegree[V];
+    for(int i=0; i<V; i++)indegree[i]=0;
+    for(int i=0; i<V; i++){
+        for(auto it:adj[i]){
+            indegree[it]++;
+        }
+    }
+    queue<int> q;
+    for(int i=0; i<V; i++){
+        if(indegree[i]==0)q.push(i);
+    }
+    vector<int> topo;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        topo.push_back(node);
+        for(auto it:adj[node]){
+            indegree[it]--;
+            if(indegree[it]==0)q.push(it);
+        }
+    }
+    return topo;
+}
+ 
+bool palindrome(ll n){
+    string num=to_string(n);
+    int l=0,r=num.size()-1;
+    while(l<r){
+        if(num[l]!=num[r])return false;
+        l++;
+        r--;
+    }
+    return true;
+}
+ 
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> deg(n+1,0);
+    for(int i=1; i<n; i++){
+        int u, v;
+        cin>>u>>v;
+        deg[u]++;
+        deg[v]++;
+    }
+    string s;
+    cin>>s;
+    int cnt0=0,cnt1=0,clq=0,ciq=0;
+    for(int i=2; i<=n; i++){
+        if(deg[i]==1){
+            if(s[i-1]=='0')cnt0++;
+            else if(s[i-1]=='1')cnt1++;
+            else clq++;
+        }
+        else{
+            if(s[i-1]=='?')ciq++;
+        }
+    }
+    if(s[0]!='?'){
+        cout<<((s[0]=='1'?cnt0:cnt1)+(clq+1)/2);
+        return;
+    }else{
+        if(cnt0==cnt1 && (ciq%2==1)){
+            cout<<(cnt0+(clq+1)/2);
+            return;
+        }else{
+            cout<<(max(cnt0,cnt1)+clq/2);
+            return;
+        }
+    }
+    return;
+}
+ 
+int main(){
+    fastio();
+    int tt=1;
+    cin>>tt;
+    while(tt--){
+        solve();
+        cout<<'
+';
+    }
+    return 0;
+}
