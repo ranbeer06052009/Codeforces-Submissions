ae37ae1720cdc3add621e744b58221e10e9309fa
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-06 17:01:22 +0530

    Add A Simple Sequence [A] from Codeforces

---
+++ 2210/A - A Simple Sequence/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod=676767677;
+ 
+void solve() {
+    int n;
+    cin>>n;
+    for(int i=n;i>0;--i){
+        cout<<i<<" ";
+    }
+ 
+}
+ 
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt = 1;
+    cin >> tt;
+    while (tt--) {
+        solve();
+        cout << '
+';
+    }
+}
