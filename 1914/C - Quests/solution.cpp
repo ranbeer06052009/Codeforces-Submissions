6eccec7e709b17d41b85eeb9fc78e27dfee7ddaa
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-06 17:02:55 +0530

    Add Quests [C] from Codeforces

---
+++ 1914/C - Quests/solution.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main() {
+// 	#ifndef ONLINE_JUDGE
+// 	freopen("Error.txt", "w", stderr);
+// 	#endif
+ 
+	int t = 1; 
+	cin >> t; 
+ 
+	while (t--) { 
+		int n, k;
+		cin >> n >> k; 
+ 
+		vector<int> a(n), b(n); 
+ 
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+ 
+ 
+		for (int i = 0; i < n; i++) {
+			cin >> b[i];
+		}
+ 
+		int maxi = 0;
+		int sum = 0; 
+		int ans = 0; 
+ 
+		for (int i = 0; i < min(n, k); i++) {
+			sum += a[i]; 
+			maxi = max(maxi, b[i]); 
+			ans = max(ans, sum + (k - (i + 1)) * maxi); 
+		}
+ 
+		cout << ans << endl; 
+	}
+ 
+}
