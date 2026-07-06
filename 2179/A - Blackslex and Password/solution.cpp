c18d640d4eb60880b6ec6ac4a96c83bf8be84f3f
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-06 16:49:02 +0530

    Add Blackslex and Password [A] from Codeforces

---
+++ 2179/A - Blackslex and Password/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        ll k, x;
+        cin >> k >> x;
+        cout << k*x+1 << "
+";
+    }
+    return 0;
+}
