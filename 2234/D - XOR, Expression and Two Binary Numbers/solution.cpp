1ef76cd5d0d16edf362b3538b587cbe6cf0ae3af
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-06 17:22:00 +0530

    Add XOR, Expression and Two Binary Numbers [D] from Codeforces

---
+++ 2234/D - XOR, Expression and Two Binary Numbers/solution.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ll=long long;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+int lcm(int a, int b) {
+    return (a*b)/__gcd(a, b);
+}
+// Returns gcd(a, b) and updates x and y to solve: ax + by = gcd(a, b)
+int extGCD(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extGCD(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+    g= extGCD(abs(a), abs(b), x0, y0);
+    if (c%g!= 0) {
+        return false;
+    }
+    
+    x0*=c/g;
+    y0*=c/g;
+    if(a<0)x0= -x0;
+    if(b<0)y0= -y0;
+    return true;
+}
+ 
+//Topological Sort
+vector<int> topoSort(int &V, vector<vector<int>> adj){
+    int indegree[V];
+    for(int i=0; i<V; i++)indegree[i]=0;
+    for(int i=0; i<V; i++){
+        for(auto it:adj[i]){
+            indegree[it]++;
+        }
+    }
+    queue<int> q;
+    for(int i=0; i<V; i++){
+        if(indegree[i]==0)q.push(i);
+    }
+    vector<int> topo;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        topo.push_back(node);
+        for(auto it:adj[node]){
+            indegree[it]--;
+            if(indegree[it]==0)q.push(it);
+        }
+    }
+    return topo;
+}
+ 
+bool palindrome(ll n){
+    string num=to_string(n);
+    int l=0,r=num.size()-1;
+    while(l<r){
+        if(num[l]!=num[r])return false;
+        l++;
+        r--;
+    }
+    return true;
+}
+ 
+void solve(){
+    ll n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    string z;
+    cin>>z;
+    ll x1=0,x2=0,x3=0;
+    for(int i=0; i<n; i++){
+        if(s[i]=='1')x1++;
+        if(z[i]=='1')x2++;
+        if(s[i]!=z[i])x3++;
+    }
+    ll c1=x1*(n-x1);
+    ll c2=x2*(n-x2);
+    ll c3=x3*(n-x3);
+    
+    ll len=(1LL<<k)+1;
+    ll q=len/3;
+ 
+    ll f1,f2,f3;
+    if(k%2==0){
+        f1=q+1;
+        f2=q+1;
+        f3=q;
+    }else{
+        f1=q;f2=q;f3=q;
+    }
+    ll ans= f1*c1+f2*c2+f3*c3;
+    cout<<ans;
+    return;
+}
+ 
+int main(){
+    fastio();
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<'
+';
+    }
+    return 0;
+}
