63bbe9429d038b8c35b1165686a351d4452c824d
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-06 16:45:48 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+int main() {
+    int w;
+    std::cin >> w;
+    if (w > 2 && w % 2 == 0) {
+        std::cout << "YES" << std::endl;
+    } else {
+        std::cout << "NO" << std::endl;
+    }
+    return 0;
+}
