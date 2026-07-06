3b047d21527357d8a7cff3cac2c15aad5629040c
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-06 17:33:42 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+void solve(){
+    ll n, m, i, j;
+    cin >>n >>m >>i>>j;
+    cout<<1 <<" " <<1 <<" " <<n <<" " <<m <<"
+";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
