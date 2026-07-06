7124cbe8617c9e8feec2864b060227640b33fd78
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-06 17:35:13 +0530

    Add Colorful Table [C] from Codeforces

---
+++ 1870/C - Colorful Table/solution.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ll=long long;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+// int lcm(int a, int b) {
+//     return (a*b)/__gcd(a, b);
+// }
+// // Returns gcd(a, b) and updates x and y to solve: ax + by = gcd(a, b)
+// int extGCD(int a, int b, int &x, int &y) {
+//     if (b == 0) {
+//         x = 1;
+//         y = 0;
+//         return a;
+//     }
+//     int x1, y1;
+//     int d = extGCD(b, a % b, x1, y1);
+//     x = y1;
+//     y = x1 - y1 * (a / b);
+//     return d;
+// }
+// bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+//     g= extGCD(abs(a), abs(b), x0, y0);
+//     if (c%g!= 0) {
+//         return false;
+//     }
+    
+//     x0*=c/g;
+//     y0*=c/g;
+//     if(a<0)x0= -x0;
+//     if(b<0)y0= -y0;
+//     return true;
+// }
+ 
+// //Topological Sort
+// vector<int> topoSort(int &V, vector<vector<int>> adj){
+//     int indegree[V];
+//     for(int i=0; i<V; i++)indegree[i]=0;
+//     for(int i=0; i<V; i++){
+//         for(auto it:adj[i]){
+//             indegree[it]++;
+//         }
+//     }
+//     queue<int> q;
+//     for(int i=0; i<V; i++){
+//         if(indegree[i]==0)q.push(i);
+//     }
+//     vector<int> topo;
+//     while(!q.empty()){
+//         int node=q.front();
+//         q.pop();
+//         topo.push_back(node);
+//         for(auto it:adj[node]){
+//             indegree[it]--;
+//             if(indegree[it]==0)q.push(it);
+//         }
+//     }
+//     return topo;
+// }
+ 
+// bool palindrome(ll n){
+//     string num=to_string(n);
+//     int l=0,r=num.size()-1;
+//     while(l<r){
+//         if(num[l]!=num[r])return false;
+//         l++;
+//         r--;
+//     }
+//     return true;
+// }
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    vector<int>a(n);
+    for(int i=0; i<n; i++)cin>>a[i];
+    vector<int> b(k+1,n),c(k+1,-1);
+    vector<bool> vis(k+1,false);
+    for(int i=0; i<n; i++){
+        b[a[i]]=min(b[a[i]],i);
+        c[a[i]]=max(c[a[i]],i);
+        vis[a[i]]=true;
+    }
+    vector<int> ans(k+1,0);
+    int mini=n,maxi=-1;
+    for(int i=k; i>=1; i--){
+        mini=min(mini,b[i]);
+        maxi=max(maxi,c[i]);
+        if(vis[i]){
+            ans[i]=2*(maxi-mini+1);
+        }
+    }
+    for(int i=1; i<=k; i++){
+        cout<<ans[i]<<" ";
+    }
+    return;
+}
+ 
+int main(){
+    fastio();
+    int tt=1;
+    cin>>tt;
+    while(tt--){
+        solve();
+        cout<<'
+';
+    }
+    return 0;
+}
