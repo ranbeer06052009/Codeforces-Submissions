7f7f2149add2d8f51883c002793be0b3fcaa05b0
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-09 22:20:00 +0530

    Add Bogosort [B] from Codeforces

---
+++ 1312/B - Bogosort/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a.rbegin(), a.rend());
+ 
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << "
+";
+}
+ 
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    
+    return 0;
+}
