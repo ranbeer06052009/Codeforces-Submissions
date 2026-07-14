af9a2bf9317d4d1ca605508f30d20702bf9caa92
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-14 08:54:03 +0530

    Add Three Indices [A] from Codeforces

---
+++ 1380/A - Three Indices/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+ 
+    for (int i = 1; i < n - 1; i++) {
+        if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+            cout << "YES
+";
+            cout << i << " " << i + 1 << " " << i + 2 << "
+";
+            return;
+        }
+    }
+ 
+    cout << "NO
+";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
