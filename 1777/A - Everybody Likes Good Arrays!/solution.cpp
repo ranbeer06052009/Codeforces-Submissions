2e68ac65f4bffbb4c3d9d0cd0a2543b227f8f8db
Ranbeer Singh <tridev06052009@gmail.com>
2026-07-24 23:58:55 +0530

    Add Everybody Likes Good Arrays! [A] from Codeforces

---
+++ 1777/A - Everybody Likes Good Arrays!/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t; 
+    cin >> t;
+    while (t--)
+    {
+        long long n; 
+        cin >> n;
+        long long a[n]; 
+        for (long long i = 0; i < n; i++) 
+            cin >> a[i];
+        long long operations_count = 0; 
+ 
+        for (long long i = 0; i < n - 1; i++) 
+        {
+            if ((a[i] % 2) == (a[i + 1] % 2))
+                operations_count++; 
+        }
+        cout << operations_count << endl; 
+    }
+    return 0;
+}
