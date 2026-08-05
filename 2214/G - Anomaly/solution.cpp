92939c8e872a48544753a2878a6af0479ec02982
Ranbeer Singh <tridev06052009@gmail.com>
2026-08-05 22:30:08 +0530

    Add Anomaly [G] from Codeforces

---
+++ 2214/G - Anomaly/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s = "bigchadjeff";
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int i;
+        cin >> i;
+        cout << s[i-1] << '
+';
+    }
+    return 0;
+}
